ef669338fb948932c033859c6e6dbd65ec3c5cf3
KiptooMannu <Kkgg7241@gmail.com>
2024-05-31 09:44:38 +0300

    selected different elements and assigned variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const filter = document.querySelector(".filter-sub");
+const main = document.querySelector("main");
+const clear = document.querySelector(".clear");
+const header = document.querySelector("header");
